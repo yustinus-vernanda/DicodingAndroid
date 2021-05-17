bae9810eb2a5e895c8d4a4226efb75fc5769193c
yustinus-vernanda <yustinusv@gmail.com>
2021-05-17 21:36:55 +0700

    Add Card View Recycler View

--- app/src/main/java/com/dicoding/barvolume/RecyclerViewActivity.kt
+++ app/src/main/java/com/dicoding/barvolume/RecyclerViewActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ class RecyclerViewActivity : AppCompatActivity() {
 
     private lateinit var rvHeroes: RecyclerView
     private var list:ArrayList<Hero> = arrayListOf()
+    private var title: String = "Mode List"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,9 @@ class RecyclerViewActivity : AppCompatActivity() {
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
         list.addAll(HeroesData.listData)
+        title = "Mode List"
+
+        setActionBarTitle(title)
         showRecyclerList()
     }
 
@@ -40,19 +45,34 @@ class RecyclerViewActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList()
             }
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
             R.id.action_cardview -> {
+                title = "Mode CardView"
+                cardViewRecyclerGrid()
             }
         }
+        setActionBarTitle(title)
     }
 
     private fun showRecyclerGrid() {
         rvHeroes.layoutManager = GridLayoutManager(this, 2)
         val gridHeroAdapter = GridHeroAdapter(list)
         rvHeroes.adapter = gridHeroAdapter
+    }
+
+    private fun cardViewRecyclerGrid() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val gridHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
     }
 }
